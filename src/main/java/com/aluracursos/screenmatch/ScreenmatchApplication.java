29ff2a6b63c5f333423512c2474b82173e703ac7
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-21 20:01:23 -0500

    - Se importó la clase principal. - Se instancia la clase Principal y se llama al método muestraElMenu para interactuar con el usuario.

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -21,22 +22,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+thrones&apikey=66d123fa");
-		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
-		json = consumoApi.obtenerDatos("https://omdbapi.com/?t=game-of-thrones&Season=1&episode=1&apikey=66d123fa");
-		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodios);
-
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
-			json = consumoApi.obtenerDatos("https://omdbapi.com/?t=game-of-thrones&Season="+i+"&apikey=66d123fa");
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
+		// Crea una instancia de la clase Principal, que maneja la lógica de búsqueda de series
+		Principal principal = new Principal();
+		// Llama al método muestraElMenu para iniciar la interacción con el usuario
+		principal.muestraElMenu();
 	}
 }
