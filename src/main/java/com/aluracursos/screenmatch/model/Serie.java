b99ac766f2fd183243cfc19b488bdf259921ce2b
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-01 20:05:23 -0500

    refactor: migrar de record a clase Serie y agregar campos adicionales
    - Se reemplaza el record DatosSerie por una clase Serie para mayor flexibilidad.
    - Se añaden los campos `poster`, `genero`, `actores` y `sinopsis` para enriquecer la información de cada serie.
    - Se incluye el uso de la enumeración `Categoria` en el campo `genero` para una categorización más estructurada.

---
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+    private String titulo;
+    private Integer totalDeTemporadas;
+    private Double evaluaciones;
+    private String poster;
+    private Categoria genero;
+    private String actores;
+    private String sinopsis;
+}
