084b6c62249355f123288b7dfa3219947e2eda0c
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-06 14:44:22 -0500

    feat: añade método fromString al enum Categoria para convertir cadenas en instancias de categoría

--- src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -12,4 +12,23 @@ public enum Categoria {
     Categoria(String categoriaOmdb) {
         this.categoriaOmdb = categoriaOmdb;
     }
+
+    public static Categoria fromString(String text) {
+        // Verifica si el texto es nulo o vacío para evitar errores
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException("El texto proporcionado es nulo o vacío");
+        }
+
+        // Recorre todas las constantes del enum Categoria
+        for (Categoria categoria : Categoria.values()) {
+            // Compara el valor de categoriaOmdb con el texto proporcionado, ignorando mayúsculas
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria; // Retorna la constante que coincide
+            }
+        }
+
+        // Lanza una excepción si no se encuentra ninguna coincidencia
+        throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
+    }
+
 }
