95f39949465b7dd7898fc1e7cc24e9049dcd3c80
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 10:57:43 -0500

    feat: Inyectar SerieRepository en ScreenMatchApplication para manejar la persistencia de series
    - Agregado SerieRepository con @Autowired para gestionar la persistencia.
    - Actualizado el método `run` para pasar SerieRepository a la instancia de Principal.

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -2,6 +2,8 @@ package com.aluracursos.screenmatch;
 
 import com.aluracursos.screenmatch.principal.EjemploStreams;
 import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
+	// Repositorio de Series inyectado para permitir la persistencia de datos en la clase Principal
+	@Autowired
+	private SerieRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Crea una instancia de la clase Principal, que maneja la lógica de búsqueda de series
-		Principal principal = new Principal();
+		// Inicia Principal con el repositorio para que pueda guardar series en la base de datos
+		Principal principal = new Principal(repository);
 		// Llama al método muestraElMenu para iniciar la interacción con el usuario
 		principal.muestraElMenu();
 //		EjemploStreams ejemploStreams = new EjemploStreams();
