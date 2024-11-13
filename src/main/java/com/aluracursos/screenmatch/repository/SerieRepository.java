d2c6fd4b197431cd46ef0a0213896e178c9afdb8
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 18:25:35 -0500

    feat: agregar método para obtener las 5 series mejor evaluadas
    - Añadido el método `findTop5ByOrderByEvaluacionDesc` para obtener las 5 series con las mejores evaluaciones de manera descendente.

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,10 +3,13 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 // Interfaz SerieRepository para gestionar operaciones CRUD en la entidad Serie
 // Extiende JpaRepository para aprovechar métodos estándar de JPA como findAll(), save(), deleteById(), etc.
 public interface SerieRepository extends JpaRepository<Serie, Long> {
     Optional<Serie> findByTituloContainsIgnoreCase(String nombreSerie);
+
+    List<Serie> findTop5ByOrderByEvaluacionDesc();
 }
