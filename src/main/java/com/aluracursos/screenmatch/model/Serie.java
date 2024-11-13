dbc7cc9c51559ed0004709424a6bc4f2b82b0360
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 11:38:35 -0500

    Agregado constructor sin parámetros en la clase Serie para compatibilidad con JPA.

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -25,6 +25,8 @@ public class Serie {
     @Transient
     private List<Episodio> episodios;
 
+    public Serie() {}
+
     public Serie(DatosSerie datosSerie) {
         this.titulo = datosSerie.titulo();
         this.totalDeTemporadas = datosSerie.totalDeTemporadas();
