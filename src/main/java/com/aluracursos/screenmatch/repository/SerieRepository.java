39430232876bf9ce452c63e6d20e99883c8ad972
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 18:09:38 -0500

    feat: agregar método de búsqueda por título en SerieRepository
    - Añadido el método `findByTituloContainsIgnoreCase` para realizar búsquedas de series por título con coincidencias parciales, ignorando mayúsculas y minúsculas.

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,7 +3,10 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // Interfaz SerieRepository para gestionar operaciones CRUD en la entidad Serie
 // Extiende JpaRepository para aprovechar métodos estándar de JPA como findAll(), save(), deleteById(), etc.
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainsIgnoreCase(String nombreSerie);
 }
