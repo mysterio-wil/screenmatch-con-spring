fc8fc4728a602d651737a30c557c9a511b985d85
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 10:36:04 -0500

    feat: Crear paquete 'repository' y añadir la interfaz 'SerieRepository' para gestión de entidades Serie
    - Creado el paquete 'repository' para organizar las interfaces de acceso a datos.
    - Añadida la interfaz 'SerieRepository', extendiendo JpaRepository para proporcionar CRUD y operaciones adicionales en la entidad Serie.

---
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,9 @@
+package com.aluracursos.screenmatch.repository;
+
+import com.aluracursos.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Interfaz SerieRepository para gestionar operaciones CRUD en la entidad Serie
+// Extiende JpaRepository para aprovechar métodos estándar de JPA como findAll(), save(), deleteById(), etc.
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+}
