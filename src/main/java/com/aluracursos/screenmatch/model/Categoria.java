af4c63665ae1cae476fa381e4b76ecccfee9acb5
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-13 19:38:56 -0500

    feat: agregar soporte para búsqueda de categorías en español
    - Se ha actualizado el enum `Categoria` para incluir un campo adicional `categoriaEspanol` que representa el nombre de la categoría en español.
    - Se implementó un nuevo método `fromEspanol` que permite convertir un nombre de categoría en español a su valor correspondiente en el enum.
    - Se mantuvo el método `fromString` para seguir soportando la conversión desde la categoría en inglés.
    - Ahora el sistema puede manejar tanto las categorías en inglés como en español para realizar búsquedas de series por género.

--- src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -1,16 +1,17 @@
 package com.aluracursos.screenmatch.model;
 
 public enum Categoria {
-    ACCION("Action"),
-    ROMANCE("Romance"),
-    COMEDIA("Comedy"),
-    DRAMA("Drama"),
-    CRIMEN("Crime");
+    ACCION("Action", "Acción"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comedia"),
+    DRAMA("Drama", "Drama"),
+    CRIMEN("Crime", "Crimen");
 
     private String categoriaOmdb;
-
-    Categoria(String categoriaOmdb) {
+    private String categoriaEspanol;
+    Categoria(String categoriaOmdb, String categoriaEspanol) {
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol = categoriaEspanol;
     }
 
     public static Categoria fromString(String text) {
@@ -31,4 +32,21 @@ public enum Categoria {
         throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
     }
 
+    public static Categoria fromEspanol(String text) {
+        // Verifica si el texto es nulo o vacío para evitar errores
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException("El texto proporcionado es nulo o vacío");
+        }
+
+        // Recorre todas las constantes del enum Categoria
+        for (Categoria categoria : Categoria.values()) {
+            // Compara el valor de categoriaOmdb con el texto proporcionado, ignorando mayúsculas
+            if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
+                return categoria; // Retorna la constante que coincide
+            }
+        }
+
+        // Lanza una excepción si no se encuentra ninguna coincidencia
+        throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
+    }
 }
