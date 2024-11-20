a397e85882f839afcd6b02025355cd3981381bf8
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-20 16:10:58 -0500

    Integrado el repositorio SerieRepository en el endpoint GET /series
    - Se utilizó el repositorio SerieRepository para obtener las series desde la base de datos y devolverlas en el endpoint GET /series.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,20 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repository;
 
     @GetMapping("/series")
-    public String mostrarMensaje() {
-        return "Este es mi primer mensaje en mi aplicación web";
+    public List<Serie> obtenerTodasLasSeries() {
+        return repository.findAll();
     }
 }
