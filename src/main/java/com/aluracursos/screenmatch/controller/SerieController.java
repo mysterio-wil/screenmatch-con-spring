7bf4369100e584006a3f7a993f5aa754cd493ea6
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-22 18:10:45 -0500

    Actualizado el controlador SerieController para usar SerieDTO
    - El método 'obtenerTodasLasSeries()' ahora retorna una lista de SerieDTO en lugar de las entidades Serie.
    - Implementada la transformación de las entidades Serie a SerieDTO utilizando Streams y el método 'map()'.
    - Mejora en la estructura de la respuesta del endpoint GET /series para desacoplar la capa de datos de la representación externa.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,12 +1,13 @@
 package com.aluracursos.screenmatch.controller;
 
-import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -14,7 +15,9 @@ public class SerieController {
     private SerieRepository repository;
 
     @GetMapping("/series")
-    public List<Serie> obtenerTodasLasSeries() {
-        return repository.findAll();
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
+                        s.getGenero(), s.getActores(), s.getSinopsis())).collect(Collectors.toList());
     }
 }
