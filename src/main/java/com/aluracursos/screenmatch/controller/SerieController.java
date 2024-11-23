62a46c92e2f7c2f45bb27954f64181fbb183b98c
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-23 13:42:52 -0500

    Se añadió en SerieController los endpoints GET /series/{id}/temporadas/todas y GET /series/{id}/temporadas/{numeroTemporada}.
    - El endpoint /series/{id}/temporadas/todas permite obtener todas las temporadas de una serie específica mediante su ID.
    - El endpoint /series/{id}/temporadas/{numeroTemporada} permite obtener los episodios de una temporada específica de una serie utilizando el ID de la serie y el número de la temporada.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,7 +1,7 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.EpisodioDTO;
 import com.aluracursos.screenmatch.dto.SerieDTO;
-import com.aluracursos.screenmatch.repository.SerieRepository;
 import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,5 +36,15 @@ public class SerieController {
     @GetMapping("/{id}")
     public SerieDTO obtenerPorId(@PathVariable Long id) {
         return servicio.obtenerPorId(id);
+    }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obtenerTodasLasTemporadas(@PathVariable Long id) {
+        return servicio.obtenerTodasLasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numeroTemporada}")
+    public List<EpisodioDTO> obtenerTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numeroTemporada) {
+        return servicio.obtenerTemporadasPorNumero(id, numeroTemporada);
     }
 }
