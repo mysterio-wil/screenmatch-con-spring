24aa06b81dc65c0a75fdd41d8c71224c73aab676
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-23 13:31:30 -0500

    Se renombró Long Id por Long id en el record SerieDTO

--- src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
+++ src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
@@ -1,11 +1,9 @@
 package com.aluracursos.screenmatch.dto;
 
 import com.aluracursos.screenmatch.model.Categoria;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 public record SerieDTO(
-        Long Id,
+        Long id,
         String titulo,
         Integer totalDeTemporadas,
         Double evaluacion,
