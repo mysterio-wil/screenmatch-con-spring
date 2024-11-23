f772c25de92a8fcfddf1ad812e4d31ce59a55d4c
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-22 20:01:40 -0500

    Refactorizada la clase SerieService para incluir nuevos métodos
    - Se refactorizó el método `obtenerTodasLasSeries()` para usar el método `convierteDatos()`, que convierte las entidades `Serie` a `SerieDTO`.
    - Se añadió el método `obtenerTop5()` para obtener las 5 mejores series basadas en la evaluación, usando el repositorio.
    - Se implementó el método `convierteDatos()` para convertir una lista de entidades `Serie` a `SerieDTO`.

--- src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package com.aluracursos.screenmatch.service;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,15 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obtenerTodasLasSeries() {
-        return repository.findAll().stream()
-                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
+        return convierteDatos(repository.findAll());
+    }
+
+    public List<SerieDTO> obtenerTop5() {
+        return convierteDatos(repository.findTop5ByOrderByEvaluacionDesc());
+    }
+
+    public List<SerieDTO> convierteDatos(List<Serie> serie) {
+        return serie.stream().map(s -> new SerieDTO(s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
                         s.getGenero(), s.getActores(), s.getSinopsis())).collect(Collectors.toList());
     }
 }
