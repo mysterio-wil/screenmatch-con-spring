0a5b63f01484bb06b610ae0d474e76b101fbf13c
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-22 20:58:26 -0500

    Añadido el método obtenerLanzamientosMasRecientes en SerieService
    - Se añadió el método `obtenerLanzamientosMasRecientes()` en la clase `SerieService` para obtener las series más recientes utilizando el repositorio.

--- src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -22,8 +22,12 @@ public class SerieService {
         return convierteDatos(repository.findTop5ByOrderByEvaluacionDesc());
     }
 
+    public List<SerieDTO> obtenerLanzamientosMasRecientes() {
+        return convierteDatos(repository.lanzamientosMasRecientes());
+    }
+
     public List<SerieDTO> convierteDatos(List<Serie> serie) {
-        return serie.stream().map(s -> new SerieDTO(s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
+        return serie.stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
                         s.getGenero(), s.getActores(), s.getSinopsis())).collect(Collectors.toList());
     }
 }
