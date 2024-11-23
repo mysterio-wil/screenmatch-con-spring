5e785d4fe48e1cf82016a81aed737b33399fcb7e
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-22 19:30:17 -0500

    Refactorizada la clase SerieController para utilizar SerieService
    - Se actualizó el controlador 'SerieController' para delegar la lógica de negocio a 'SerieService'.
    - El método 'obtenerTodasLasSeries()' ahora llama al servicio 'SerieService' para obtener las series, mejorando la separación de responsabilidades.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -2,6 +2,7 @@ package com.aluracursos.screenmatch.controller;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.repository.SerieRepository;
+import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,10 @@ import java.util.stream.Collectors;
 @RestController
 public class SerieController {
     @Autowired
-    private SerieRepository repository;
+    private SerieService servicio;
 
     @GetMapping("/series")
     public List<SerieDTO> obtenerTodasLasSeries() {
-        return repository.findAll().stream()
-                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalDeTemporadas(), s.getEvaluacion(), s.getPoster(),
-                        s.getGenero(), s.getActores(), s.getSinopsis())).collect(Collectors.toList());
+        return servicio.obtenerTodasLasSeries();
     }
 }
