257484611d5bb9dff9c72a144b2a395e9e37d7a9
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-23 18:36:14 -0500

    **Se añadió el endpoint GET `/series/{id}/temporadas/top` en la clase `SerieController`.**
    - Este endpoint permite obtener los 5 episodios mejor evaluados de una serie específica mediante su ID.
    - El método delega la lógica de negocio al método `obtenerTopEpisodios` en `SerieService`.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -52,4 +52,9 @@ public class SerieController {
     public List<SerieDTO> obtenerSeriesPorCategoria(@PathVariable String nombreGenero) {
         return servicio.obtenerSeriesPorCategoria(nombreGenero);
     }
+
+    @GetMapping("/{id}/temporadas/top")
+    public List<EpisodioDTO> obtenerTopEpisodios(@PathVariable Long id) {
+        return servicio.obtenerTopEpisodios(id);
+    }
 }
